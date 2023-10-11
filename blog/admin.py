37f0647cc97c8584c8736806baae0e9fabd60508
Patt Höge <paty.hoge98@gmail.com>
2023-10-11 19:22:27 +0000

    add admin comment panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-# Register your models here.
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('created_on', 'approved')
+    search_fields = ('name', 'email address', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
